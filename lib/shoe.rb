df40154fa2d991ec0c5a1ddf2abdbe99eaf9272d
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-04 11:31:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -44,3 +44,17 @@ def cobble
   end
 end
 
+
+class Shoe
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
