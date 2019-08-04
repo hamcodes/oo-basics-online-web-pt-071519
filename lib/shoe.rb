636c38378bdc8c0beea09bcf5ce23bdb0c7873b2
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-04 11:33:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -46,8 +46,8 @@ end
 
 
 class Shoe
-  attr_accessor :color, :size, :material
-  attr_reader :title
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
  
   def initialize(title)
     @title = title
