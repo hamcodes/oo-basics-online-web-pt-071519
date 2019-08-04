07d7b07ceb6c7199632deb9c966b0b2a14fdda24
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-04 11:33:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,56 +1,56 @@
-class Shoe
-    def initialize(brand)
-    @brand = brand
-  end
+# class Shoe
+#     def initialize(brand)
+#     @brand = brand
+#   end
   
-  def brand
-    @brand
-end
-  def color=(color)
-    @color = color
-end
+#   def brand
+#     @brand
+# end
+#   def color=(color)
+#     @color = color
+# end
  
-  def color
-    @color
-  end
+#   def color
+#     @color
+#   end
   
-    def size=(size)
-    @size = size
-end
+#     def size=(size)
+#     @size = size
+# end
  
-  def size
-    @size
-  end
+#   def size
+#     @size
+#   end
   
-    def material=(material)
-    @material = material
-end
+#     def material=(material)
+#     @material = material
+# end
  
-  def material
-    @material
-  end
+#   def material
+#     @material
+#   end
   
-    def condition=(condition)
-    @condition = condition
-end
+#     def condition=(condition)
+#     @condition = condition
+# end
  
-  def condition
-    @condition
-  end
+#   def condition
+#     @condition
+#   end
 
-def cobble
-  puts "Your shoe is as good as new!"
-      @condition = "new"
-  end
-end
+# def cobble
+#   puts "Your shoe is as good as new!"
+#       @condition = "new"
+#   end
+# end
 
 
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand= brand
   end
  
   def turn_page
